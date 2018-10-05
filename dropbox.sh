945a7b93d53e6fcabb44344472fc8cf2f5507ebb
血衫非弧 <admin@kelu.org>
2018-10-05 10:11:42 +0800

    [update] change the status command

--- dropbox.sh
+++ dropbox.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 if [ $1 ]; then
-docker run --rm \
-  -v /root/Dropbox:/root/Dropbox \
-  -v /root/.dropbox:/root/.dropbox \
-  -v /root/.dropbox-dist:/root/.dropbox-dist \
-  kelvinblood/dropbox $1
+  docker exec dropbox python /dropbox.py $1
 else
 docker run --name=dropbox -d \
   --entrypoint=/.dropbox-dist/dropboxd \
